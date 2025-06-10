048e8256f9af72a476a759038c6132cef3caf126
shenjoung <84434661+shenjoung@users.noreply.github.com>
2025-06-10 15:41:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 git init
-git remote add origin https://github.com/你的帳號/你的repo.git
+git remote add origin https://github.com/shenjoung/codextest001.git
 git add .
 git commit -m "Initial commit"
 git push -u origin main
